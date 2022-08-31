c8e5f59e4e65663d99bf65fdc2c1e92ef5849ffa
kathelan <mateuszdeptulski@gmail.com>
2022-08-31 18:11:13 +0200

    initial commit

---
+++ src/main/java/pl/kathelan/mdbackend/MdBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.kathelan.mdbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MdBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/kathelan/mdbackend/MdBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kathelan.mdbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
